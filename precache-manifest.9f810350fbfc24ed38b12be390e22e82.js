be597fcf12221b9c715463434a623da3ae311695
ZakirMuhammad <muhammad.zakir@axisrooms.com>
2018-10-28 00:50:11 +0530

    Updates

---
+++ precache-manifest.9f810350fbfc24ed38b12be390e22e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a14d08bf710814bf70",
+    "url": "/properties-list/static/js/runtime~main.a6a14d08.js"
+  },
+  {
+    "revision": "56f149cf631b5dfdf6bb",
+    "url": "/properties-list/static/js/main.56f149cf.chunk.js"
+  },
+  {
+    "revision": "25264e41863be1523e97",
+    "url": "/properties-list/static/js/1.25264e41.chunk.js"
+  },
+  {
+    "revision": "56f149cf631b5dfdf6bb",
+    "url": "/properties-list/static/css/main.12a390a4.chunk.css"
+  },
+  {
+    "revision": "25264e41863be1523e97",
+    "url": "/properties-list/static/css/1.d2eb56cb.chunk.css"
+  },
+  {
+    "revision": "1c9edd862d286d1e4778d1e783011567",
+    "url": "/properties-list/index.html"
+  }
+];
